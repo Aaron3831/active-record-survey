da50b38c8c5741d1240514260285d7dd7c0e3ffa
Aaron Nguyen and Jesse Hoare Evans <jesse@>
2017-05-09 16:12:56 -0700

    add more routing for update function

--- app.rb
+++ app.rb
@@ -7,78 +7,91 @@ require("./lib/question")
 require("pg")
 
 get("/") do
-  @answers = Answer.all
   erb(:index)
 end
 
-get('/designer') do
+get('/questions') do
   @questions = Question.all()
-  erb(:designer)
+  erb(:questions)
 end
 
-get("/questions/new") do
+get('/questions/new') do
+  @questions = Question.all()
+  erb(:questions)
+end
+
+get('/questions/:id') do
+  @question = Question.find(params.fetch("id").to_i())
+  @answers = @question.answers()
+  # @answers.save()
   erb(:question)
 end
 
-post("/designer") do
-  prompt = params.fetch("prompt")
-  question = Question.new({:prompt => prompt, :id => nil})
+post('/answer/:id') do
+  id = params.fetch('id').to_i
+  @question = Question.find(id)
+  response = params.fetch("response")
+  @question.answers().create({:response => response})
+  # @question.answers().new({:prompt => prompt})
+  # @question.save()
+  @answers = Answer.all()
+  # @answers.save()
+  erb(:success)
+end
+
+post('/questions') do
+  prompt = params.fetch('prompt')
+  question = Question.new({:prompt => prompt})
   question.save()
   erb(:designer_success)
 end
 
-get('/answers')do
-  @answers = Answer.all()
+get('/questions/:id/edit') do
+  @question = Question.find(params.fetch("id").to_i())
+  @answers = @question.answers()
   erb(:question)
 end
 
-get("/questions/:id") do
-  @questions = Question.find(params.fetch("id").to_i())
-  erb(:answer_form)
-end
 
-post('/answers') do
-  erb(:designer_success)
-end
-
+#BELOW WAS A MAJOR HEADACHE
 post('/answers/:id') do
   id = params.fetch('id')
   @question = Question.find(id)
-  response = params.fetch("response")
-  @question.answers().new({:response => response})
-  @question.save()
-  @answer = Answer.all()
-  erb(:designer_success)
+  prompt = params.fetch("prompt")
+  @question.answers().create({:response => prompt})
+  # @question.answers().new({:prompt => prompt})
+  # @question.save()
+  @answers = Answer.all()
+  erb(:success)
 end
 
-get("/questions/:id/edit") do
+#ABOVE WAS A MAJOR HEADACH
+get('/answers/:id/edit') do
+  @answer = Answer.find(params.fetch('id').to_i())
+  erb(:answer_edit)
+end
+
+patch("/answers/:id") do
+  prompt = params.fetch("prompt")
+  @answer = Answer.find(params.fetch("id").to_i())
+  @answer.update({:prompt => prompt})
+  @answers = Answer.all()
+  erb(:index)
+end
+
+get("/quetions/:id/edit") do
   @question = Question.find(params.fetch("id").to_i())
+  erb(:questions)
+end
+
+get("/question_edit") do
+  @question = Question.find(params.fetch('id').to_i())
   erb(:question_edit)
 end
 
 patch("/questions/:id") do
-  name = params.fetch("name")
+  name = params.fetch("prompt")
   @question = Question.find(params.fetch("id").to_i())
-  @question.update({:name => name})
-  erb(:question)
-end
-
-delete("/questions/:id") do
-  @question = Question.find(params.fetch("id").to_i())
-  @question.delete()
-  @questions = Question.all()
-  erb(:index)
-end
-
-get('/answers/:id/edit') do
-  @answer = Answer.find(params.fetch("id").to_i())
-  erb(:answer_form)
-end
-
-patch("/answers/:id") do
-  response = params.fetch("response")
-  @answer = Answer.find(params.fetch("id").to_i())
-  @answer.update({:response => response})
-  @answers = Answer.all()
-  erb(:index)
+  @question.update({:prompt => name})
+  erb(:questions)
 end
